bd4dec38232e3021ecb33cd169b8fd70ff744030
hemelbala19-cell <hemelbala19@gmail.com>
2026-08-27 11:19:52 +0600

    Replace Firebase config with placeholder values
    Updated Firebase configuration with placeholder values.

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDsgQJgZ64-ARLqKa3vcdQpu_xVupD90W4",
-    authDomain: "apexify-72bc2.firebaseapp.com",
-    projectId: "apexify-72bc2",
-    storageBucket: "apexify-72bc2.firebasestorage.app",
-    messagingSenderId: "114061131405",
-    appId: "1:114061131405:web:ffd00b701d70c164976cc4",
-    measurementId: "G-RB6KW4S7JL"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
